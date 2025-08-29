96158237737f9b2cfd1119ac847ea89765c56138
Gabe <gabepineda6@gmail.com>
2025-08-28 20:14:50 -0700

    created filter to handle and authenticate incoming requests ensuring we only autherticate if need be and one time.

--- src/main/java/com/example/book/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/book/config/JwtAuthenticationFilter.java
@@ -1,4 +1,66 @@
 package com.example.book.config;
 
-public class JwtAuthenticationFilter {
+import com.example.book.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final HandlerExceptionResolver handlerExceptionResolver;
+    private final JwtService jwtService;
+    private UserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter(HandlerExceptionResolver handlerExceptionResolver, JwtService jwtService, UserDetailsService userDetailsService) {
+        this.handlerExceptionResolver = handlerExceptionResolver;
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        //check if we need auth for this request
+        final String authHeader = request.getHeader("Authorization");
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+        }
+
+        //request needs auth
+
+        try{
+            final String jwt = authHeader.substring(7); //no need to check if null as we check case at strt
+            final String userEmail = jwtService.extractUserName(jwt); //getting username from subject
+
+            //avoid re authenticating request by checking username and sercurity context
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if(userEmail != null && authentication == null){
+                //load user
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+                //validate token
+                if(jwtService.isTokenValid(jwt, userDetails)){
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
+            filterChain.doFilter(request,response);
+        }catch (Exception e){
+            handlerExceptionResolver.resolveException(request,response,null, e);
+        }
+    }
 }
