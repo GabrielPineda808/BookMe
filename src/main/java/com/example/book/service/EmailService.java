8cc8c3a83fcadc6091843ecdc5ddf2483d633eac
Gabe <gabepineda6@gmail.com>
2025-08-29 14:15:15 -0700

    Email service to handle sending verification emails

--- src/main/java/com/example/book/service/EmailService.java
+++ src/main/java/com/example/book/service/EmailService.java
@@ -1,4 +1,30 @@
 package com.example.book.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailService {
+    private final JavaMailSender javaMailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender javaMailSender){
+        this.javaMailSender = javaMailSender;
+    }
+
+    //Send verification email
+    public void sendVerificationEmail(String to, String subject, String text) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text, true);
+
+        javaMailSender.send(message);
+    }
 }
