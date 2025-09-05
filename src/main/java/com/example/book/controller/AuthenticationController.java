856727ebb37f75a03022af142580439ba31dfadc
Gabe <gabepineda6@gmail.com>
2025-09-05 16:19:59 -0700

    Auth service created to handle signing up users, verifying user by email, and resending verification emails

--- src/main/java/com/example/book/controller/AuthenticationController.java
+++ src/main/java/com/example/book/controller/AuthenticationController.java
@@ -1,4 +1,49 @@
 package com.example.book.controller;
 
+import com.example.book.dto.RegisterUserDto;
+import com.example.book.dto.VerifyUserDto;
+import com.example.book.model.User;
+import com.example.book.service.AuthenticationService;
+import com.example.book.service.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/auth")
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthenticationController {
+    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto){
+        User registeredUser = authenticationService.signup(registerUserDto);
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<?> verifyUser(@RequestBody VerifyUserDto verifyUserDto){
+        try {
+            authenticationService.verifyUser(verifyUserDto);
+            return ResponseEntity.ok("Account Verified Successfully");
+        }catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/resend")
+    public ResponseEntity<?> resend(@RequestParam String email){
+        try {
+            authenticationService.resendVerification(email);
+            return ResponseEntity.ok("Verification Code Resent");
+        }catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
 }
