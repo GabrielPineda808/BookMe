38d7d5bc79d4ccb731cbe8389f7fae5822eda3a5
Gabe <gabepineda6@gmail.com>
2025-09-05 16:26:10 -0700

    hard coded constructor

--- src/main/java/com/example/book/service/UserService.java
+++ src/main/java/com/example/book/service/UserService.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
