c7a5797ed2cbe194dd111ccd8b35a64409065801
Gabe <gabepineda6@gmail.com>
2025-09-05 16:24:23 -0700

    dto for verifying users

--- src/main/java/com/example/book/dto/VerifyUserDto.java
+++ src/main/java/com/example/book/dto/VerifyUserDto.java
@@ -8,4 +8,20 @@ import lombok.Setter;
 public class VerifyUserDto {
     private String email;
     private String verification_code;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getVerification_code() {
+        return verification_code;
+    }
+
+    public void setVerification_code(String verification_code) {
+        this.verification_code = verification_code;
+    }
 }
