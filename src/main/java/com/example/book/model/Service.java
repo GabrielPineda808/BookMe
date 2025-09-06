a3e55ed545d4b132a37cbdce27e7ebf6e3c981d3
Gabe <gabepineda6@gmail.com>
2025-09-05 18:09:29 -0700

    created status entity with one to one relationship to an address

--- src/main/java/com/example/book/model/Service.java
+++ src/main/java/com/example/book/model/Service.java
@@ -7,20 +7,74 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(name="u")
-public class Business{
+@Table(name="service")
+public class Service {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int id;
+    int serviceId;
 
-    private String ownerId;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
+    @Column(nullable = false)
     private String handle;
 
-    private String name;
+    @Column(nullable = false)
+    private String service_name;
 
-    private int location;
+    @OneToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
 
-    private String timezone;
+    public Service() {
+    }
 
+    public Service( User user, String handle, String service_name, Location location) {
+
+        this.user = user;
+        this.handle = handle;
+        this.service_name = service_name;
+        this.location = location;
+    }
+
+    public int getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(int serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getHandle() {
+        return handle;
+    }
+
+    public void setHandle(String handle) {
+        this.handle = handle;
+    }
+
+    public String getService_name() {
+        return service_name;
+    }
+
+    public void setService_name(String service_name) {
+        this.service_name = service_name;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
 }
