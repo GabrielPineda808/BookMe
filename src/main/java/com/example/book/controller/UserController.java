777c90e429bdeae798bb60948466c79bd1eb91e9
Gabe <gabepineda6@gmail.com>
2025-09-07 16:52:08 -0700

    created method to get all bookings

--- src/main/java/com/example/book/controller/UserController.java
+++ src/main/java/com/example/book/controller/UserController.java
@@ -1,4 +1,32 @@
 package com.example.book.controller;
 
+import com.example.book.dto.RegisterUserDto;
+import com.example.book.model.Booking;
+import com.example.book.model.User;
+import com.example.book.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
+@PreAuthorize("hasRole('USER')")
+@CrossOrigin
 public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllBookings(@RequestBody String email){
+        List<Booking> bookings = userService.getAllBookings(email);
+        return ResponseEntity.ok(bookings);
+    }
+
+
 }
