77141af402afdf21e5af3db16f0c81f20baf79ff
Gabe <gabepineda6@gmail.com>
2025-09-06 20:18:25 -0700

    service repo

--- src/main/java/com/example/book/repository/ServiceRepository.java
+++ src/main/java/com/example/book/repository/ServiceRepository.java
@@ -1,4 +1,9 @@
 package com.example.book.repository;
 
-public interface ServiceRepository {
+import com.example.book.model.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceRepository extends JpaRepository<Service, Long> {
 }
