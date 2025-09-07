01eeaf58b23bd6e6ba7081a0c9ef2f8c285b432e
Gabe <gabepineda6@gmail.com>
2025-09-06 20:22:39 -0700

    created service controllers to create new services as the user

--- src/main/java/com/example/book/controller/ServiceController.java
+++ src/main/java/com/example/book/controller/ServiceController.java
@@ -1,4 +1,30 @@
 package com.example.book.controller;
 
+import com.example.book.dto.ServiceDto;
+import com.example.book.model.Service;
+import com.example.book.service.ServiceService;
+import org.springframework.context.annotation.Role;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/service")
+@PreAuthorize("hasRole('USER')")
+@CrossOrigin
 public class ServiceController {
+    private final ServiceService service;
+
+    public ServiceController(ServiceService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createService(@RequestBody ServiceDto input, @AuthenticationPrincipal(expression = "username") String email){
+        Service service1 = service.createService(input, email);
+        return ResponseEntity.ok(service1);
+    }
 }
