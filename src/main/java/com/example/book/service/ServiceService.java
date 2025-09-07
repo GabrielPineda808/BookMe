7c1e02fc6503f39a4c2b566b7d4962be523b6a5f
Gabe <gabepineda6@gmail.com>
2025-09-06 20:14:44 -0700

    created method to handle creating and savings services made by auth usrs

--- src/main/java/com/example/book/service/ServiceService.java
+++ src/main/java/com/example/book/service/ServiceService.java
@@ -1,4 +1,48 @@
 package com.example.book.service;
 
+import com.example.book.dto.ServiceDto;
+import com.example.book.model.Location;
+import com.example.book.model.User;
+import com.example.book.repository.ServiceRepository;
+import com.example.book.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ServiceService {
+    private final ServiceRepository serviceRepository;
+    private final UserRepository userRepository;
+
+    public ServiceService(ServiceRepository serviceRepository, UserRepository userRepository) {
+        this.serviceRepository = serviceRepository;
+        this.userRepository = userRepository;
+    }
+
+    public com.example.book.model.Service createService(ServiceDto input, String email){
+        User owner = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+
+        com.example.book.model.Service service = new com.example.book.model.Service();
+        service.setUser(owner);
+        service.setService_name(input.getService_name());
+        service.setHandle(input.getHandle());
+        service.setLocation(input.getLocation());
+        service.setDesc(input.getDesc());
+        service.setDesc(input.getDesc());
+        service.setOpen(input.getOpen());
+        service.setClose(input.getClose());
+        service.setInterval(input.getInterval());
+        if (input.getLocation() != null) {
+            Location loc = new Location();
+            loc.setAddress(input.getLocation().getAddress());
+            loc.setCity(input.getLocation().getCity());
+            loc.setState(input.getLocation().getState());
+            loc.setArea_code(input.getLocation().getArea_code());
+            loc.setCountry(input.getLocation().getCountry());
+            service.setLocation(loc);
+        }
+        serviceRepository.save(service);
+
+        return service;
+    }
 }
