6ebe8156846f5bec24df7846df736e581e7fa3ee
Gabe <gabepineda6@gmail.com>
2025-09-06 20:17:51 -0700

    Created response obj to return on post method for creating service

--- src/main/java/com/example/book/response/ServiceResponse.java
+++ src/main/java/com/example/book/response/ServiceResponse.java
@@ -1,4 +1,78 @@
 package com.example.book.response;
 
+import com.example.book.dto.LocationDto;
+
 public class ServiceResponse {
+    private Long id;
+    private String handle;
+    private String service_name;
+    private String desc;
+    private LocationDto location;
+    private String open;
+    private String close;
+    private int interval;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getHandle() {
+        return handle;
+    }
+
+    public void setHandle(String handle) {
+        this.handle = handle;
+    }
+
+    public String getService_name() {
+        return service_name;
+    }
+
+    public void setService_name(String service_name) {
+        this.service_name = service_name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public LocationDto getLocation() {
+        return location;
+    }
+
+    public void setLocation(LocationDto location) {
+        this.location = location;
+    }
+
+    public String getOpen() {
+        return open;
+    }
+
+    public void setOpen(String open) {
+        this.open = open;
+    }
+
+    public String getClose() {
+        return close;
+    }
+
+    public void setClose(String close) {
+        this.close = close;
+    }
+
+    public int getInterval() {
+        return interval;
+    }
+
+    public void setInterval(int interval) {
+        this.interval = interval;
+    }
 }
