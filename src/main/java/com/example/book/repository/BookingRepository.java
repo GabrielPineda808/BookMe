07934e6e588bb3bd440c1f98e3410216d01c29ae
Gabe <gabepineda6@gmail.com>
2025-09-06 20:18:55 -0700

    booking repo

--- src/main/java/com/example/book/repository/BookingRepository.java
+++ src/main/java/com/example/book/repository/BookingRepository.java
@@ -1,4 +1,10 @@
 package com.example.book.repository;
 
-public interface BookingRepository {
+import com.example.book.model.Booking;
+import com.example.book.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, User> {
 }
