276652a903f70327f0359ca3bb49de075b3e73a4
Gabe <gabepineda6@gmail.com>
2025-09-07 18:41:14 -0700

    review repo"

--- src/main/java/com/example/book/repository/ReviewRepository.java
+++ src/main/java/com/example/book/repository/ReviewRepository.java
@@ -1,4 +1,9 @@
 package com.example.book.repository;
 
-public interface ReviewRepository {
+import com.example.book.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
