261c81d34e1f6341f3d6a60303ca2f108a4b1802
Gabe <gabepineda6@gmail.com>
2025-09-08 13:52:05 -0700

    created query to validate wehter a user has already made a review for the booking and service. Only one review per user per booking

--- src/main/java/com/example/book/repository/ReviewRepository.java
+++ src/main/java/com/example/book/repository/ReviewRepository.java
@@ -2,8 +2,17 @@ package com.example.book.repository;
 
 import com.example.book.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    @Query("""
+            SELECT (count(r.id) > 0)
+            FROM Review r
+            WHERE r.booking.id = :bookingId
+            AND r.user.id = :userId
+            """)
+    boolean findByUserBooking(@Param("bookingId") Long bookingId, @Param("userId") Long userId);
 }
