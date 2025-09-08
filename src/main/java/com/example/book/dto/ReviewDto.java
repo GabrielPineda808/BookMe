d36cd781c4f21cb00e25d4d1ef54ef405d2da86f
Gabe <gabepineda6@gmail.com>
2025-09-07 18:39:20 -0700

    dto for reviews

--- src/main/java/com/example/book/dto/ReviewDto.java
+++ src/main/java/com/example/book/dto/ReviewDto.java
@@ -1,4 +1,31 @@
 package com.example.book.dto;
 
 public class ReviewDto {
+    private Long serviceId;
+    private Long rating;
+    private String comment;
+
+    public Long getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(Long serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public Long getRating() {
+        return rating;
+    }
+
+    public void setRating(Long rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
