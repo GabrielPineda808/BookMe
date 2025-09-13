95e312eebc6869de4ebfd665a4fb91e03078cbdf
Gabe <gabepineda6@gmail.com>
2025-09-12 21:55:13 -0700

    added update user endpoint

--- src/main/java/com/example/book/controller/UserController.java
+++ src/main/java/com/example/book/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.example.book.controller;
 
 import com.example.book.dto.RegisterUserDto;
+import com.example.book.dto.UserDto;
 import com.example.book.model.Booking;
 import com.example.book.model.User;
 import com.example.book.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,6 +28,12 @@ public class UserController {
     public ResponseEntity<?> getAllBookings(@RequestBody String email){
         List<Booking> bookings = userService.getAllBookings(email);
         return ResponseEntity.ok(bookings);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<?> updateUser(@RequestBody UserDto input, @AuthenticationPrincipal(expression = "username") String email){
+        User user = userService.updateUser(input, email);
+        return ResponseEntity.ok(UserDto.from(user));
     }
 
 
