beab43258ed7b7bdbede814e75feec80161bda39
Gabriel Pineda <gabepineda6@gmail.com>
2025-09-16 22:05:11 -0700

    addedVerificationCodeExpired/InvalidCode custom exceptions

--- src/main/java/com/example/book/exception/VerificationCodeExpiredException.java
+++ src/main/java/com/example/book/exception/VerificationCodeExpiredException.java
@@ -1,4 +1,12 @@
 package com.example.book.exception;
 
-public class VerificationCodeExpiredException {
+public class VerificationCodeExpiredException extends RuntimeException {
+    public VerificationCodeExpiredException(String message) {
+        super(message);
+    }
+    
+    public VerificationCodeExpiredException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
+
