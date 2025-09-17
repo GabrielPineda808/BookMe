78d14670091d1a3207514dddb49cfb7cd71cbc49
Gabe <gabepineda6@gmail.com>
2025-09-16 17:45:40 -0700

    repo for booking change request'

--- src/main/java/com/example/book/repository/BookingChangeRequestRepository.java
+++ src/main/java/com/example/book/repository/BookingChangeRequestRepository.java
@@ -1,4 +1,9 @@
 package com.example.book.repository;
 
-public class BookingChangeRequestRepository {
+import com.example.book.model.BookingChangeRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public abstract class BookingChangeRequestRepository implements JpaRepository<BookingChangeRequest,Long> {
 }
