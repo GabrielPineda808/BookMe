f321ae9949618c28744f2f51fdde05e47a904f25
Gabriel Pineda <gabepineda6@gmail.com>
2025-09-28 20:46:29 -0700

    chore(email): dto for changing email

--- src/main/java/com/example/book/dto/ChangeEmailRequestDto.java
+++ src/main/java/com/example/book/dto/ChangeEmailRequestDto.java
@@ -1,4 +1,12 @@
 package com.example.book.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ChangeEmailRequestDto {
+    @NotBlank
+    private String newEmail;
 }
